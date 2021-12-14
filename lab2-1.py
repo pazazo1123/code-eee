4ab187809b975b852c4c85b47bd9d21273a90ea9
kewalin445 <68361407+kewalin445@users.noreply.github.com>
2021-12-14 10:20:53 +0700

    first commit

---
+++ lab2-1.py
@@ -0,0 +1,12 @@
+username = input("รับค่าผู้ใช้งาน: ")
+password = input("รับค่ารหัสผ่าน: ")
+
+if username == "admin" and password == "1234" :
+  print("ยินดีต้อนรับสู่โปรแกรม")
+  print("ฟาอิซ มุคุระ")
+else :
+    print("username หรือ password ผิดพลาด!!!")
+    
+    print("สวัสดี!!!")
+    
+    
